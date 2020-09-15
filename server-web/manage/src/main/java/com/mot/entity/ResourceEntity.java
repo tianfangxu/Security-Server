17efc98aa05404b2cf4ff89021bfe0032d3d0825
tianfangxu <15755502377@163.com>
2020-09-15 14:17:37 +0800

    20200915

--- server-web/manage/src/main/java/com/mot/entity/ResourceEntity.java
+++ server-web/manage/src/main/java/com/mot/entity/ResourceEntity.java
@@ -6,11 +6,17 @@ import lombok.Data;
 public class ResourceEntity extends BaseEntity {
     private String id;
     private String resourceCode;
+    /**
+     * 字段命名规则：系统名称.操作对象.功能名称 （描述越清晰越好）
+     */
     private String resourceName;
     private String resourceUrl;
     private int sort;
     private String parentId;
     private int isLeaf;
     private int type;
-    private int state;  //暂时用不上的字段
+    /**
+     * 暂时用不上的字段
+     */
+    private int state;
 }
