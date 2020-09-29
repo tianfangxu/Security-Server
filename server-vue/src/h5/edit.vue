23436937f9a5847b351108f43fb5f7a515cea0f7
15755502377@163.com <15755502377@163.com>
2020-09-29 22:27:39 +0800

    edit1

--- server-vue/src/h5/edit.vue
+++ server-vue/src/h5/edit.vue
@@ -14,18 +14,31 @@
 		<div>
 			<el-divider><div style="color: #909399;">下面是正文</div></el-divider>
 		</div>
-		<div style="padding-right: 10px;">
-			<div>
+		<div style="padding-right: 10px;padding-bottom: 60px;">
+			<div v-for="i in imgsNumber.length">
 				<el-input
 				  type="textarea"
 				  placeholder="正文内容"
-				  v-model="contenttext" 
-				  :autosize="{ minRows: 3, maxRows: 999}" 
+				  :autosize="{ minRows: 5, maxRows: 999}" 
 				  class='edit-content-class'>
 				</el-input>
 				<div style="float: right;margin-top: -36px;position: sticky;font-size: 24px;">
-					<i class="el-icon-picture-outline" style="color: #1E90FF;"></i>
+					<el-upload 
+						:show-file-list="false"
+						action="https://jsonplaceholder.typicode.com/posts/" 
+						:on-success="handleAvatarSuccess"
+						:before-upload="beforeAvatarUpload"
+						>
+						<i class="el-icon-picture-outline" style="color: #1E90FF;"></i>
+					</el-upload>
 				</div>
+				
+				<div style="padding-left: 10px;" v-for="j in imgsNumber[i]" :key="j">
+					<el-divider v-if='j == 1'><div style="color: #909399;">插入图片</div></el-divider>
+					<el-image style="width: 100%;" :src="imgsList[j+imgsNumber[i-1]-1]" fit="cover" ></el-image>
+					<el-divider v-if='j == imgsNumber[i]'><div style="color: #909399;">插入正文</div></el-divider>
+				</div>
+				
 			</div>
 			
 		</div>
@@ -38,9 +51,28 @@
 	  	data() {
 		      return {
 		        headertext:'',
-		        contenttext:''
+		        contentList:[
+		        	{
+		        		content:'',
+		        		imgs:['../../static/img/example/44.jpg','../../static/img/example/44.jpg']
+		        	}
+		        ],
+		        imgsNumber:[0],	//这个数组的初始值位0，数组的长度表示上述正文的个数，每个下标对应的值。表示图片的个数。图片的位置为该位置前面元素的总和，例如：[0,3,5],表示：共有2个正文8涨图片，第一个正文有3张，为imgsList中的1-3；第二个正文有5张，为imgsList中的（0+3） - （0+3+5）
+		        imgsList:['../../static/img/example/44.jpg','../../static/img/example/44.jpg','../../static/img/example/44.jpg','../../static/img/example/44.jpg','../../static/img/example/44.jpg']
 		      }
 		},
+		methods:{
+			handleAvatarSuccess(res, file){
+				imgs.push(URL.createObjectURL(file.raw));
+			},
+			beforeAvatarUpload(){
+				
+			},
+			handlePictureCardPreview(file) {
+		        this.dialogImageUrl = file.url;
+		        this.dialogVisible = true;
+		    }
+		}
 	}
 </script>
 
